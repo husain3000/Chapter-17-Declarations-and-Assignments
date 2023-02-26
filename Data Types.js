76b1261a7060048e930beffcb1107ca4d010a11a
husain3000 <73273019+husain3000@users.noreply.github.com>
2023-02-26 13:37:01 +0530

    first commit

---
+++ Data Types.js
@@ -0,0 +1,16 @@
+// Number
+var length = 16;
+console.log(length)
+// String
+var message = "Hello, World!";
+console.log(message)
+// Array
+var carNames = ['Chevrolet', 'Nissan', 'BMW'];
+console.log(carNames)
+
+//object
+var person = {
+ firstName: "John",
+ lastName: "Doe"
+};
+console.log(person)
---
+++ Modifying constants.js
@@ -0,0 +1,8 @@
+const person = {
+ name: "Husain"
+};
+console.log('The name of the person is', person.name);
+person.name = "Husain";
+console.log('The name of the person is', person.name);
+person.surname = "Saify";
+console.log('The name of the person is', person.name, 'and the surname is', person.surname);
